dbba604e2dfd38cdba197486545bac5570fb39b4
Sorrel Brigman <sorrel.k@gmail.com>
2017-03-22 16:37:11 -0500

    changed content type test to what the scripts are actually returning

--- main.js
+++ main.js
@@ -98,15 +98,15 @@ const getJSON = (url) => {
 
     get(url, (res)=>{
       const statusCode = res.statusCode;
-    //   const contentType = res.headers['content-type'];
+      const contentType = res.headers['content-type'];
 
       let error;
       if(statusCode !== 200) {
         error = new Error(`Request Failed.\n Status Code: ${statusCode}`)
        }
-      // else if (!/^application\/json/.test(contentType)){
-      //   error = new Error(`Invalid content-type.\n Expected application/json but received ${contentType}`)
-      // }
+      else if (!/^text\/javascript/.test(contentType)){
+        error = new Error(`Invalid content-type.\n Expected application/json but received ${contentType}`)
+      }
 
       if (error) {
         console.log(error.message);
