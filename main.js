a62208e1229ba5e96e509d1e6ff2081128f02458
Sorrel Brigman <sorrel.k@gmail.com>
2017-03-22 16:00:29 -0500

    changed pmargins

--- main.js
+++ main.js
@@ -96,33 +96,33 @@ let stringParams = JSON.stringify(params);
 const getJSON = (url) => {
   return new Promise((resolve, reject)=>{
 
-  get(url, (res)=>{
-    const statusCode = res.statusCode;
-//   const contentType = res.headers['content-type'];
+    get(url, (res)=>{
+      const statusCode = res.statusCode;
+    //   const contentType = res.headers['content-type'];
 
-    let error;
-    if(statusCode !== 200) {
-      error = new Error(`Request Failed.\n Status Code: ${statusCode}`)
-     }
-    // else if (!/^application\/json/.test(contentType)){
-    //   error = new Error(`Invalid content-type.\n Expected application/json but received ${contentType}`)
-    // }
+      let error;
+      if(statusCode !== 200) {
+        error = new Error(`Request Failed.\n Status Code: ${statusCode}`)
+       }
+      // else if (!/^application\/json/.test(contentType)){
+      //   error = new Error(`Invalid content-type.\n Expected application/json but received ${contentType}`)
+      // }
 
-    if (error) {
-      console.log(error.message);
-      res.resume();
-      return;
-    }
+      if (error) {
+        console.log(error.message);
+        res.resume();
+        return;
+      }
 
-    let body = '';
-    res.on('data', (buff)=>{
-      body += buff.toString();
-    });
-    res.on('end', ()=>{
-      resolve(body);
+      let body = '';
+      res.on('data', (buff)=>{
+        body += buff.toString();
+      });
+      res.on('end', ()=>{
+        resolve(body);
+      })
     })
   })
- })
 }
 
 
