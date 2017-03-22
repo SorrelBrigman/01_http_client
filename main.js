cf41502850c81d2e96a2244882733df52519b75e
Sorrel Brigman <sorrel.k@gmail.com>
2017-03-22 13:42:44 -0500

    wrote query

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
-const {get, createServer} = require('http');
+'use strict'
 
-const server = createServer();
+const {get} = require('http');
+
+//Use the first argument for a ticker symbol.
+const tickerSymbol = process.argv[2];
 
 // Write a program that performs an HTTP GET request to get the average stock price.
-//Use the first argument for a ticker symbol.
-//Use the get method in the http module with the API provided by MarkitOnDemand.
 
+
+let params = {
+  Normalized : true,
+  // StartDate: "",
+  // EndDate: "",
+  DataPeriod: "Year",
+  Elements: [
+    {Symbol:tickerSymbol}
+  ]
+}
+
+let stringParams = JSON.stringify(params);
+
+get(`http://dev.markitondemand.com/Api/v2/InteractiveChart/jsonp?parameters=${stringParams}`, (res)=>{
+  console.log(res);
+})
+//Use the get method in the http module with the API provided by MarkitOnDemand.
+//markitDemand query http://dev.markitondemand.com/Api/v2/InteractiveChart?dataperiod=year&element=[symbol=${tickerSymbol},"price":["ohlc"]]
 // It would certainly be easier to test if you can grab the latest stock price,
 //but because the response is so small, there may not be an opportunity to demonstrate chunking.
 //On the API docs you will see an example request for data to create a chart.
